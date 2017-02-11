cd20de2da07d2bc576dcf241f2234d4943ca47b3
Nikolay Voronov <nkvoronov@gmail.com>
2017-02-11 22:02:10 +0200

    update

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -8,6 +8,8 @@
 '''
 
 import xbmc
+from traceback import format_exc
+import sys
 
 ADDON_ID = "script.skin.helper.widgets"
 KODI_VERSION = int(xbmc.getInfoLabel("System.BuildVersion").split(".")[0])
@@ -17,6 +19,12 @@ def log_msg(msg, loglevel=xbmc.LOGDEBUG):
     if isinstance(msg, unicode):
         msg = msg.encode('utf-8')
     xbmc.log("Skin Helper Widgets --> %s" % msg, level=loglevel)
+
+
+def log_exception(modulename, exceptiondetails):
+    '''helper to properly log an exception'''
+    log_msg(format_exc(sys.exc_info()), xbmc.LOGWARNING)
+    log_msg("Exception in %s ! --> %s" % (modulename, exceptiondetails), xbmc.LOGERROR)
 
 
 def create_main_entry(item):
